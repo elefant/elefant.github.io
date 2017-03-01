6d05ceef1a943466b72d2675ebc7dcca2490e199
elefant <hchang@mozilla.com>
2017-03-02 00:03:48 +0800

    Create q3.js

---
+++ enil/q3.js
@@ -0,0 +1,16 @@
+function Solution3(n) {
+  // f(n) = 0 for n < 0
+  //        1 for n == 0
+  //        f(n-1) + ... f(n-6)
+  
+  var sol = new Array(n + 1); // sol[i] => f(i)
+  sol[0] = 1; // f(0) = 1
+  for (var i = 1; i <= n; i++) {
+  	sol[i] = 0;
+    for (var j = 1; j <= 6; j++) {
+    	sol[i] += (i - j >= 0 ? sol[i - j] : 0);
+    }
+  }
+  
+  return sol[n];
+}
