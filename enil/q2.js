e025a1495635ff09da8379155d56e6e2b6ec0c2a
elefant <hchang@mozilla.com>
2017-03-02 00:03:27 +0800

    Create q2.js

---
+++ enil/q2.js
@@ -0,0 +1,29 @@
+// function Node() {
+//   this.left = this.right = null; // to other Node.
+//   this.value = 0;
+// }
+function Solution2(root) {
+  if (!root) {
+  	return [];
+  }
+  
+  var sol = []; // sol[i] contains nodes which depth are i. Root's depth is 0.
+  var q = [root];
+  while (q.length !== 0) {
+  	sol.push(q.slice(0)); // Push the clone 'nextLevelQ' to sol.
+  
+    var nextLevelQ = [];
+    q.forEach(function(n) {
+    	if (n.left) {
+      	nextLevelQ.push(n.left);
+      }
+      if (n.right) {
+        nextLevelQ.push(n.right);
+      }
+    });
+    
+    q = nextLevelQ;
+  }
+  
+  return sol;
+}
